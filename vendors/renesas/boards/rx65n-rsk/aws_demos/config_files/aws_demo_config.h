51f2fadf3f6d6d8d3b11bb9f8f0a0b2373f2893f
Carl Lundin <lundinc@amazon.com>
2020-01-02 11:17:48 -0800

    Added demo metrics to release candidate.

--- vendors/renesas/boards/rx65n-rsk/aws_demos/config_files/aws_demo_config.h
+++ vendors/renesas/boards/rx65n-rsk/aws_demos/config_files/aws_demo_config.h
@@ -69,6 +69,21 @@
 #define democonfigGREENGRASS_DISCOVERY_TASK_STACK_SIZE    ( configMINIMAL_STACK_SIZE * 16 )
 #define democonfigGREENGRASS_DISCOVERY_TASK_PRIORITY      ( tskIDLE_PRIORITY )
 
+#define democonfigMEMORY_ANALYSIS
+
+#ifdef democonfigMEMORY_ANALYSIS
+    #define democonfigMEMORY_ANALYSIS_STACK_DEPTH_TYPE    UBaseType_t
+    #define democonfigMEMORY_ANALYSIS_MIN_EVER_HEAP_SIZE()        xPortGetMinimumEverFreeHeapSize()
+    #if ( INCLUDE_uxTaskGetStackHighWaterMark == 1 )
+        /* Convert from stack words to bytes */
+        #define democonfigMEMORY_ANALYSIS_STACK_WATERMARK( x )    uxTaskGetStackHighWaterMark( x ) * ( uint32_t ) sizeof( StackType_t ); /*lint !e961 Casting is not redundant on smaller architectures. */
+    #else
+        #define democonfigMEMORY_ANALYSIS_STACK_WATERMARK( x )    NULL
+    #endif /* if( INCLUDE_uxTaskGetStackHighWaterMark == 1 ) */
+#endif /* democonfigMEMORY_ANALYSIS */
+
+
+
 /* Workaround for the incompatibility between GNU/IAR C compilers and the CC-RX compiler. */
 #if defined( __CCRX__ )
     #define __FUNCTION__    __func__

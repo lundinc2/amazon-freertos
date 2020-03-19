e1d3e34206762e6bc6dbdc914e0fc75566334324
Carl Lundin <lundinc@amazon.com>
2020-03-19 12:21:47 -0700

    Removed provisioning from microchip.

--- vendors/microchip/boards/curiosity_pic32mzef/aws_demos/application_code/main.c
+++ vendors/microchip/boards/curiosity_pic32mzef/aws_demos/application_code/main.c
@@ -117,7 +117,7 @@ void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
             /* A simple example to demonstrate key and certificate provisioning in
              * microcontroller flash using PKCS#11 interface. This should be replaced
              * by production ready key provisioning mechanism. */
-            vDevModeKeyProvisioning();
+            //vDevModeKeyProvisioning();
 
             /* Run all demos. */
             DEMO_RUNNER_RunDemos();

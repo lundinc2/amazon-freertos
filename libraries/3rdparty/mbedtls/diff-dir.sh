9732e1f1b184e830aa35f99d307a411b0869b39b
Carl Lundin <lundinc@amazon.com>
2020-04-07 12:43:31 -0700

    Saving scripts

---
+++ libraries/3rdparty/mbedtls/diff-dir.sh
@@ -0,0 +1,1 @@
+diff -w --strip-trailing-cr -q -y --suppress-common-lines -r -d $1 ~/Documents/mbedtls/$1
---
+++ libraries/3rdparty/mbedtls/patch.sh
@@ -0,0 +1,4 @@
+diff -u ~/Documents/amazon-freertos/libraries/3rdparty/mbedtls/include/mbedtls/$1 ~/Documents/mbedtls/include/mbedtls/$1 > patchfile.patch
+patch ~/Documents/amazon-freertos/libraries/3rdparty/mbedtls/include/mbedtls/$1 patchfile.patch 
+rm patchfile.patch
+echo "Done patching"

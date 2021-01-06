9c3154a239e7f45e0323a56588f0a23dc93c5a2b
Lee George Thomas <ltham@amazon.com>
2021-01-05 16:31:07 -0800

    use coreHTTP for HTTP request to get GGCore details

--- demos/greengrass_connectivity/aws_greengrass_discovery_demo.c
+++ demos/greengrass_connectivity/aws_greengrass_discovery_demo.c
@@ -52,6 +52,12 @@
 #include "platform/iot_network_freertos.h"
 #include "platform/iot_clock.h"
 
+/* Common HTTP demo utilities. */
+#include "http_demo_utils.h"
+
+/* Transport interface implementation include header for TLS. */
+#include "transport_secure_sockets.h"
+
 /* Greengrass includes. */
 #include "aws_ggd_config.h"
 #include "aws_ggd_config_defaults.h"
@@ -59,6 +65,9 @@
 
 /* Includes for initialization. */
 #include "iot_mqtt.h"
+
+/* Include header for root CA certificates. */
+#include "iot_default_root_certificates.h"
 
 #define ggdDEMO_MAX_MQTT_MESSAGES              3
 #define ggdDEMO_MAX_MQTT_MSG_SIZE              500
@@ -70,6 +79,21 @@
 /* Number of times to try MQTT connection. */
 #define ggdDEMO_NUM_TRIES                      3
 #define ggdDEMO_RETRY_WAIT_MS                  2000
+
+/**
+ * @brief The length in bytes of the user buffer.
+ */
+#define democonfigUSER_BUFFER_LENGTH           ( 3500 )
+
+/**
+ * @brief HTTP command to retrieve JSON file from the Cloud.
+ */
+#define ggdCLOUD_DISCOVERY_ADDRESS      \
+    "GET /greengrass/discover/thing/%s" \
+    " HTTP/1.1\r\n\r\n"
+
+#define ggdDEMOHTTP_PATH    "/greengrass/discover/thing/" clientcredentialIOT_THING_NAME
+
 
 /**
  * @brief Contains the user data for callback processing.
@@ -89,7 +113,25 @@ typedef struct
  * long enough for the TLS negotiation to complete. */
 static const uint32_t _maxCommandTimeMs = 20000UL;
 static const uint32_t _timeBetweenPublishMs = 1500UL;
-static char pcJSONFile[ ggdDEMO_DISCOVERY_FILE_SIZE ];
+
+/**
+ * @brief A buffer used in the demo for storing HTTP request headers and
+ * HTTP response headers and body.
+ *
+ * @note This demo shows how the same buffer can be re-used for storing the HTTP
+ * response after the HTTP request is sent out. However, the user can also
+ * decide to use separate buffers for storing the HTTP request and response.
+ */
+static uint8_t ucUserBuffer[ democonfigUSER_BUFFER_LENGTH ];
+
+/**
+ * @brief Connect to HTTP server with reconnection retries.
+ *
+ * @param[out] pxNetworkContext The output parameter to return the created network context.
+ *
+ * @return pdPASS on successful connection, pdFAIL otherwise.
+ */
+static BaseType_t prvConnectToServer( NetworkContext_t * pxNetworkContext );
 
 static IotMqttError_t _mqttConnect( GGD_HostAddressData_t * pxHostAddressData,
                                     const IotNetworkInterface_t * pNetworkInterface,
@@ -202,25 +244,257 @@ static IotMqttError_t _mqttConnect( GGD_HostAddressData_t * pxHostAddressData,
 
 /*-----------------------------------------------------------*/
 
+static BaseType_t prvConnectToServer( NetworkContext_t * pxNetworkContext )
+{
+    ServerInfo_t xServerInfo = { 0 };
+    SocketsConfig_t xSocketsConfig = { 0 };
+    BaseType_t xStatus = pdPASS;
+    TransportSocketStatus_t xNetworkStatus = TRANSPORT_SOCKET_STATUS_SUCCESS;
+
+    /* Initializer server information. */
+    xServerInfo.pHostName = clientcredentialMQTT_BROKER_ENDPOINT;
+    xServerInfo.hostNameLength = strlen( clientcredentialMQTT_BROKER_ENDPOINT );
+    xServerInfo.port = clientcredentialGREENGRASS_DISCOVERY_PORT;
+
+    /* Configure credentials for TLS mutual authenticated session. */
+    xSocketsConfig.enableTls = true;
+    xSocketsConfig.pAlpnProtos = NULL;
+    xSocketsConfig.maxFragmentLength = 0;
+    xSocketsConfig.disableSni = false;
+    xSocketsConfig.pRootCa = tlsATS1_ROOT_CERTIFICATE_PEM;
+    xSocketsConfig.rootCaSize = sizeof( tlsATS1_ROOT_CERTIFICATE_PEM );
+    xSocketsConfig.sendTimeoutMs = 1000;
+    xSocketsConfig.recvTimeoutMs = 1000;
+
+    /* Establish a TLS session with the HTTP server. This example connects to
+     * the HTTP server as specified in democonfigAWS_IOT_ENDPOINT and
+     * democonfigAWS_HTTP_PORT in http_demo_mutual_auth_config.h. */
+    IotLogInfo( "Establishing a TLS session to %.*s:%d.",
+                ( int32_t ) strlen( clientcredentialMQTT_BROKER_ENDPOINT ),
+                clientcredentialMQTT_BROKER_ENDPOINT,
+                clientcredentialGREENGRASS_DISCOVERY_PORT );
+
+    /* Attempt to create a mutually authenticated TLS connection. */
+    xNetworkStatus = SecureSocketsTransport_Connect( pxNetworkContext,
+                                                     &xServerInfo,
+                                                     &xSocketsConfig );
+
+    if( xNetworkStatus != TRANSPORT_SOCKET_STATUS_SUCCESS )
+    {
+        xStatus = pdFAIL;
+    }
+
+    return xStatus;
+}
+
+/*-----------------------------------------------------------*/
+
+static BaseType_t prvSendHttpRequest( const TransportInterface_t * pxTransportInterface,
+                                      const char * pcMethod,
+                                      size_t xMethodLen,
+                                      const char * pcPath,
+                                      size_t xPathLen,
+                                      char ** ppcJSONFile,
+                                      uint32_t * plJSONFileLength )
+{
+    /* Return value of this method. */
+    BaseType_t xStatus = pdPASS;
+
+    /* Configurations of the initial request headers that are passed to
+     * #HTTPClient_InitializeRequestHeaders. */
+    HTTPRequestInfo_t xRequestInfo;
+    /* Represents a response returned from an HTTP server. */
+    HTTPResponse_t xResponse;
+    /* Represents header data that will be sent in an HTTP request. */
+    HTTPRequestHeaders_t xRequestHeaders;
+
+    /* Return value of all methods from the HTTP Client library API. */
+    HTTPStatus_t xHTTPStatus = HTTPSuccess;
+
+    configASSERT( pcMethod != NULL );
+    configASSERT( pcPath != NULL );
+
+    /* Initialize all HTTP Client library API structs to 0. */
+    ( void ) memset( &xRequestInfo, 0, sizeof( xRequestInfo ) );
+    ( void ) memset( &xResponse, 0, sizeof( xResponse ) );
+    ( void ) memset( &xRequestHeaders, 0, sizeof( xRequestHeaders ) );
+
+    /* Initialize the request object. */
+    xRequestInfo.pHost = clientcredentialMQTT_BROKER_ENDPOINT;
+    xRequestInfo.hostLen = strlen( clientcredentialMQTT_BROKER_ENDPOINT );
+    xRequestInfo.pMethod = pcMethod;
+    xRequestInfo.methodLen = xMethodLen;
+    xRequestInfo.pPath = pcPath;
+    xRequestInfo.pathLen = xPathLen;
+
+    /* Set "Connection" HTTP header to "keep-alive" so that multiple requests
+     * can be sent over the same established TCP connection. */
+    xRequestInfo.reqFlags = HTTP_REQUEST_KEEP_ALIVE_FLAG;
+
+    /* Set the buffer used for storing request headers. */
+    xRequestHeaders.pBuffer = ucUserBuffer;
+    xRequestHeaders.bufferLen = democonfigUSER_BUFFER_LENGTH;
+
+    xHTTPStatus = HTTPClient_InitializeRequestHeaders( &xRequestHeaders,
+                                                       &xRequestInfo );
+
+    if( xHTTPStatus == HTTPSuccess )
+    {
+        /* Initialize the response object. The same buffer used for storing
+         * request headers is reused here. */
+        xResponse.pBuffer = ucUserBuffer;
+        xResponse.bufferLen = democonfigUSER_BUFFER_LENGTH;
+
+        IotLogInfo( "Sending HTTP %.*s request to %.*s%.*s...",
+                    ( int32_t ) xRequestInfo.methodLen, xRequestInfo.pMethod,
+                    ( int32_t ) strlen( clientcredentialMQTT_BROKER_ENDPOINT ),
+                    clientcredentialMQTT_BROKER_ENDPOINT,
+                    ( int32_t ) xRequestInfo.pathLen,
+                    xRequestInfo.pPath );
+        IotLogDebug( "Request Headers:\n%.*s\n"
+                     "Request Body:\n%.*s\n",
+                     ( int32_t ) xRequestHeaders.headersLen,
+                     ( char * ) xRequestHeaders.pBuffer );
+
+        /* Send the request and receive the response. */
+        xHTTPStatus = HTTPClient_Send( pxTransportInterface,
+                                       &xRequestHeaders,
+                                       NULL,
+                                       0,
+                                       &xResponse,
+                                       0 );
+    }
+    else
+    {
+        IotLogError( "Failed to initialize HTTP request headers: Error=%s.",
+                     HTTPClient_strerror( xHTTPStatus ) );
+    }
+
+    if( xHTTPStatus == HTTPSuccess )
+    {
+        LogInfo( ( "Received HTTP response from %.*s%.*s...\n",
+                   ( int32_t ) strlen( clientcredentialMQTT_BROKER_ENDPOINT ),
+                   clientcredentialMQTT_BROKER_ENDPOINT,
+                   ( int32_t ) xRequestInfo.pathLen,
+                   xRequestInfo.pPath ) );
+        LogInfo( ( "Response Headers:\n%.*s\n",
+                   ( int32_t ) xResponse.headersLen, xResponse.pHeaders ) );
+        LogInfo( ( "Status Code:\n%u\n",
+                   xResponse.statusCode ) );
+        LogDebug( ( "Response Body Length:%d\n",
+                    ( int32_t ) xResponse.bodyLen ) );
+        LogDebug( ( "Response Body:\n%.*s\n",
+                    ( int32_t ) xResponse.bodyLen, xResponse.pBody ) );
+
+        /* Set the output parameters. */
+        *ppcJSONFile = xResponse.pBody;
+        *plJSONFileLength = xResponse.bodyLen;
+    }
+    else
+    {
+        LogError( ( "Failed to send HTTP %.*s request to %.*s%.*s: Error=%s.",
+                    ( int32_t ) xRequestInfo.methodLen, xRequestInfo.pMethod,
+                    ( int32_t ) strlen( clientcredentialMQTT_BROKER_ENDPOINT ),
+                    clientcredentialMQTT_BROKER_ENDPOINT,
+                    ( int32_t ) xRequestInfo.pathLen, xRequestInfo.pPath,
+                    HTTPClient_strerror( xHTTPStatus ) ) );
+    }
+
+    if( xHTTPStatus != HTTPSuccess )
+    {
+        xStatus = pdFAIL;
+    }
+
+    return xStatus;
+}
+
+/*-----------------------------------------------------------*/
+
 static int _discoverGreengrassCore( const IotNetworkInterface_t * pNetworkInterface )
 {
     int status = EXIT_SUCCESS;
+    BaseType_t xDemoStatus = pdFAIL;
     IotMqttError_t mqttStatus = IOT_MQTT_SUCCESS;
     GGD_HostAddressData_t xHostAddressData;
     IotMqttConnection_t mqttConnection = IOT_MQTT_CONNECTION_INITIALIZER;
+    /* The transport layer interface used by the HTTP Client library. */
+    TransportInterface_t xTransportInterface;
+    /* The network context for the transport layer interface. */
+    NetworkContext_t xNetworkContext = { 0 };
+    BaseType_t xIsConnectionEstablished = pdFALSE;
+    char * pcJSONFile;
+    uint32_t ulJSONFileLength = 0UL;
 
     memset( &xHostAddressData, 0, sizeof( xHostAddressData ) );
 
     /* Demonstrate automated connection. */
     IotLogInfo( "Attempting automated selection of Greengrass device\r\n" );
 
-    if( GGD_GetGGCIPandCertificate( clientcredentialMQTT_BROKER_ENDPOINT,
-                                    clientcredentialGREENGRASS_DISCOVERY_PORT,
-                                    clientcredentialIOT_THING_NAME,
-                                    pcJSONFile,
-                                    ggdDEMO_DISCOVERY_FILE_SIZE,
-                                    &xHostAddressData )
-        == pdPASS )
+    /**************************** Connect. ******************************/
+
+    /* Attempt to connect to the HTTP server. If connection fails, retry
+     * after a timeout. The timeout value will be exponentially increased
+     * until either the maximum number of attempts or the maximum timeout
+     * value is reached. The function returns pdFAIL if the TCP connection
+     * cannot be established with the broker after the configured number of
+     * attempts. */
+    xDemoStatus = connectToServerWithBackoffRetries( prvConnectToServer,
+                                                     &xNetworkContext );
+
+    if( xDemoStatus == pdPASS )
+    {
+        /* Set a flag indicating that a TLS connection exists. */
+        xIsConnectionEstablished = pdTRUE;
+
+        /* Define the transport interface. */
+        xTransportInterface.pNetworkContext = &xNetworkContext;
+        xTransportInterface.send = SecureSocketsTransport_Send;
+        xTransportInterface.recv = SecureSocketsTransport_Recv;
+
+        IotLogInfo( "Connection established....\r\n" );
+    }
+    else
+    {
+        /* Log error to indicate connection failure after all
+         * reconnect attempts are over. */
+        IotLogError( "Failed to connect to HTTP server %.*s.",
+                     ( int32_t ) strlen( clientcredentialMQTT_BROKER_ENDPOINT ),
+                     clientcredentialMQTT_BROKER_ENDPOINT );
+    }
+
+    /* Send HTTP requst to retrieve the JSON.*/
+    if( xDemoStatus == pdPASS )
+    {
+        xDemoStatus = prvSendHttpRequest( &xTransportInterface,
+                                          HTTP_METHOD_GET,
+                                          strlen( HTTP_METHOD_GET ),
+                                          ggdDEMOHTTP_PATH,
+                                          strlen( ggdDEMOHTTP_PATH ),
+                                          &pcJSONFile,
+                                          &ulJSONFileLength );
+    }
+
+    /* Disconnect the connection from the AWS IoT broker endpoint. */
+    if( xIsConnectionEstablished == pdTRUE )
+    {
+        SecureSocketsTransport_Disconnect( &xNetworkContext );
+    }
+
+    if( xDemoStatus == pdPASS )
+    {
+        xDemoStatus = GGD_GetIPandCertificateFromJSON( pcJSONFile,
+                                                       ulJSONFileLength,
+                                                       NULL,
+                                                       &xHostAddressData,
+                                                       pdTRUE );
+    }
+
+    if( xDemoStatus == pdPASS )
+    {
+        LogInfo( ( "Host Address obtained is %s:%d", xHostAddressData.pcHostAddress, xHostAddressData.usPort ) );
+    }
+
+    if( xDemoStatus == pdPASS )
     {
         IotLogInfo( "Greengrass device discovered." );
         mqttStatus = _mqttConnect( &xHostAddressData, pNetworkInterface, &mqttConnection );

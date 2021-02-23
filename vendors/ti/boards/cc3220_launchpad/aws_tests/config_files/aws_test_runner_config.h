9fd8f983e34b14c6cf49c30cd5d317ce0d56d6fa
Carl Lundin <lundinc@amazon.com>
2021-02-23 15:19:47 -0800

    Remove unnessary files.

--- vendors/ti/boards/cc3220_launchpad/aws_tests/config_files/aws_test_runner_config.h
+++ vendors/ti/boards/cc3220_launchpad/aws_tests/config_files/aws_test_runner_config.h
@@ -49,7 +49,6 @@
 #define testrunnerFULL_CRYPTO_ENABLED               testrunnerUNSUPPORTED
 
 /* Supported tests. 0 = Disabled, 1 = Enabled */
-<<<<<<< HEAD:vendors/ti/boards/cc3220_launchpad/aws_tests/config_files/aws_test_runner_config.h
 #define testrunnerFULL_TASKPOOL_ENABLED             0
 #define testrunnerFULL_MQTT_STRESS_TEST_ENABLED     0
 #define testrunnerFULL_MQTT_AGENT_ENABLED           0
@@ -71,29 +70,5 @@
 #define testrunnerFULL_CORE_MQTT_AWS_IOT_ENABLED    0
 #define testrunnerFULL_CORE_HTTP_ENABLED            0
 #define testrunnerFULL_CORE_HTTP_AWS_IOT_ENABLED    0
-=======
-#define testrunnerFULL_TASKPOOL_ENABLED            0
-#define testrunnerFULL_MQTT_STRESS_TEST_ENABLED    0
-#define testrunnerFULL_MQTT_AGENT_ENABLED          0
-#define testrunnerFULL_MQTTv4_ENABLED              0
-#define testrunnerFULL_TCP_ENABLED                 0
-#define testrunnerFULL_GGD_ENABLED                 0
-#define testrunnerFULL_GGD_HELPER_ENABLED          0
-#define testrunnerFULL_SHADOW_ENABLED              0
-#define testrunnerFULL_SHADOWv4_ENABLED            0
-#define testrunnerFULL_WIFI_ENABLED                0
-#define testrunnerFULL_MEMORYLEAK_ENABLED          0
-#define testrunnerFULL_TLS_ENABLED                 0
-#define testrunnerFULL_OTA_AGENT_ENABLED           0
-#define testrunnerFULL_OTA_PAL_ENABLED             0
-#define testrunnerFULL_POSIX_ENABLED               0
-#define testrunnerFULL_HTTPS_CLIENT_ENABLED        0
-#define testrunnerFULL_DEVICE_SHADOW_ENABLED       0
-#define testrunnerFULL_CORE_MQTT_ENABLED           0
-#define testrunnerFULL_CORE_MQTT_AWS_IOT_ENABLED   1
-#define testrunnerFULL_CORE_HTTP_ENABLED           0
-#define testrunnerFULL_CORE_HTTP_AWS_IOT_ENABLED   0
-#define testrunnerFULL_COMMON_IO_ENABLED           0
->>>>>>> 7656b37e2 (Copy TI branch over to clean main branch fork):vendors/ti/boards/CC3220SF_LAUNCHXL/aws_tests/config_files/aws_test_runner_config.h
 
 #endif /* AWS_TEST_RUNNER_CONFIG_H */
